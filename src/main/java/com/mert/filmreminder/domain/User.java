6b01e417dd7dd187050785d3fc480028a4a24a9e
Mert <m.mert_ozdogan_@hotmail.com>
2019-01-27 23:56:53 +0100

    Added user entity #closes #3

---
+++ src/main/java/com/mert/filmreminder/domain/User.java
@@ -0,0 +1,27 @@
+package com.mert.filmreminder.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+	@Id()
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String password;
+
+	@OneToMany
+	private Set<Film> watchedFilm = new HashSet<>();
+	@OneToMany
+	private Set<Film> willingToWatchFilms = new HashSet<>();
+}
