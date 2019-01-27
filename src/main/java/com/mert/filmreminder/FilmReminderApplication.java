054373d5ab044e07a2f474ca6d2dda7495e6d0be
Mert <m.mert_ozdogan_@hotmail.com>
2019-01-27 22:22:16 +0100

    packet name change.

--- src/main/java/com/mert/filmreminder/FilmReminderApplication.java
+++ src/main/java/com/mert/filmreminder/FilmReminderApplication.java
@@ -1,4 +1,4 @@
-package com.mert.FilmReminder;
+package com.mert.filmreminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
