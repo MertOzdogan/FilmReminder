16d47009ef009084b4daf597ad9acedd60400309
Mert <m.mert_ozdogan_@hotmail.com>
2019-01-28 23:53:30 +0100

    Username added to User entity. #closes #8

--- src/main/java/com/mert/filmreminder/domain/User.java
+++ src/main/java/com/mert/filmreminder/domain/User.java
@@ -15,9 +15,10 @@ import lombok.Data;
 @Data
 public class User {
 
-	@Id()
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String username;
 	private String password;
 
 	@OneToMany
